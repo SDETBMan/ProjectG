3eb99964b0c61a970462a54ce8f6cd2b0536207a
Brian <Brian@10.0.0.67>
2024-01-03 19:22:11 -0700

    Initial Commit

---
+++ src/g/DemoG.java
@@ -0,0 +1,12 @@
+package g;
+
+public class DemoG {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Sunday");
+		System.out.println("Sprint 1...");
+		System.out.println("Sprint 2...");
+	}
+
+}
